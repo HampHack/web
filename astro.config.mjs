998a277491cb9be287e1483ce1fddb57eb5c203a
Jack Merrill <me@jackmerrill.com>
2022-11-01 21:40:16 -0400

    feat: add analytics, fix seo and tab order

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 import react from "@astrojs/react";
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), react()]
+  integrations: [tailwind(), react(), robotsTxt()]
 });
