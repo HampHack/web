136e1e4cf6e0c0b3f9c91502f55a668e1c74a4cb
Jack Merrill <me@jackmerrill.com>
2023-02-02 16:03:27 -0500

    feat: update astro and registration page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import react from "@astrojs/react";
-import robotsTxt from 'astro-robots-txt'
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), robotsTxt()]
+  integrations: [tailwind(), react(), robotsTxt()] 
 });
