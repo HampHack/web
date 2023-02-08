1952860c5816172cac13d4e1dbfa5ea58b8dfe21
Jack <me@jackmerrill.com>
2023-02-08 12:46:29 -0500

    feat: add images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import react from "@astrojs/react";
-import robotsTxt from 'astro-robots-txt';
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), robotsTxt()] 
+  integrations: [tailwind(), react(), robotsTxt()],
 });
