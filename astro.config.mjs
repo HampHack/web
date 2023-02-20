773f29bdfe56764bbdd8f2e65b36d402c90a6746
Jack <me@jackmerrill.com>
2023-02-20 13:20:31 -0500

    fix: add site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://hamphack.hampshire.edu",
   integrations: [
     tailwind(),
     react(),
